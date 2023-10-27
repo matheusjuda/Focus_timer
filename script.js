4c131ce4cc80a391bbee43015739870c274e0af0
matheusjuda <matheusbj582@gmail.com>
2023-10-27 10:55:31 -0300

    adicionando audio aos botoes, cronometros, objeto (Data)

--- script.js
+++ script.js
@@ -5,27 +5,51 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
 
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null
+
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) { // Se o musica estiver com a proriedade paused (true) de play.
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco')
     focoBt.classList.add('active') //ao ativarmos o evento click, estamos adicionando a classe active nos botões.
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 
 function alterarContexto(contexto) {
+    mostrarTempo();
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active') //quando outro botão é ativado removemos a classe conforme o "contexto"
     })
@@ -52,3 +76,42 @@ function alterarContexto(contexto) {
     }
 }
 
+const contagemRegressiva = () => {  // 
+    if (tempoDecorridoEmSegundos <= 0) { // Interrompe a contagem quando chega a 0.
+        audioTempoFinalizado.play();
+        zerar();
+        alert('Tempo finalizado!');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar) // Se ouver o click, charmar a função.
+
+function iniciarOuPausar() {
+    if(intervaloId){ // Se intervaloId tiver algum valor pausa o audio e atrinui null ao valor.
+        audioPausa.play();
+        zerar();
+        return;
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000) // setInterval vai executar a contagemRegressiva a cada 1 segundo.
+    iniciarOuPausarBt.textContent = "Pausar";
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`);
+}
+
+function zerar() { // atribui a intervaloId o valor null.
+    clearInterval(intervaloId); // interrompe a execução do codico.
+    iniciarOuPausarBt.textContent = "Começar";
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`);
+    intervaloId = null;
+}
+
+function mostrarTempo() { //mostra o tempo na tela
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'}) //Formata o cronometro
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo(); // chamando ela no escopo global para aparecer o tempo todo;
